fa2d38fb41ec6495ee3417eff65d149888a1d16f
MoeSinon <jojo@vanbi.tk>
2023-03-02 09:01:37 +0800

    0

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 const SocialShare = [
-  {
-    iconName: "fa fa-facebook",
-    link: "https://www.facebook.com/",
-  },
-  { iconName: "fa fa-twitter", link: "https://twitter.com/" },
-  {
-    iconName: "fa fa-youtube",
-    link: "https://www.youtube.com/",
-  },
-  { iconName: "fa fa-dribbble", link: "https://dribbble.com/" },
+  // {
+  //   iconName: "fa fa-facebook",
+  //   link: "https://www.facebook.com/",
+  // },
+  // { iconName: "fa fa-twitter", link: "https://twitter.com/" },
+  // {
+  //   iconName: "fa fa-youtube",
+  //   link: "https://www.youtube.com/",
+  // },
+  // { iconName: "fa fa-dribbble", link: "https://dribbble.com/" },
 ];
 
 const Social = () => {
